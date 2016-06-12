cbf91a420ea1fbebdd4ff42baff5e29c9ea88e75
syntaxSizer <kakuma4never@gmail.com>
2016-06-12 04:29:12 +0300

    creating a  hash key for the app

--- app/src/main/java/matrial/aka/facebook/MainActivity.java
+++ app/src/main/java/matrial/aka/facebook/MainActivity.java
@@ -1,13 +1,38 @@
 package matrial.aka.facebook;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends Application {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        // making hash key for the app
+try {
+    PackageInfo info = getPackageManager().getPackageInfo("matrial.aka.facebook", PackageManager.GET_SIGNATURES);
+for (Signature signature: info.signatures){
+    MessageDigest md = MessageDigest.getInstance("SHA");
+    md.update(signature.toByteArray());
+    Log.e("this is your keyHash :", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+}
+
+} catch (PackageManager.NameNotFoundException e) {
+    e.printStackTrace();
+} catch (NoSuchAlgorithmException e) {
+    e.printStackTrace();
+}
     }
 }
