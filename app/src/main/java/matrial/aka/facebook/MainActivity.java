43cfc8a993f671b82555e4f6349817c9ebcc51f5
syntaxSizer <kakuma4never@gmail.com>
2016-06-12 13:51:13 +0300

    add tracking code

--- app/src/main/java/matrial/aka/facebook/MainActivity.java
+++ app/src/main/java/matrial/aka/facebook/MainActivity.java
@@ -13,11 +13,15 @@ import com.facebook.appevents.AppEventsLogger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+// when extending application it means what ever being applied to the application
+// wil be applied to the application first instance [when you lunch the app hs will class will run]
+
 public class MainActivity extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // this code for tracking the number of time the app opens and installs
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
         // making hash key for the app
